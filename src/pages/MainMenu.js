a394300a8189740fad444e914416a41ef0b3de5f
Japsz <benjamin.meneses.14@sansano.usm.cl>
2020-09-29 19:05:03 -0300

    Responsive Main

--- src/pages/MainMenu.js
+++ src/pages/MainMenu.js
@@ -2,26 +2,24 @@ import React from 'react'
 import {Typography, Button} from '@material-ui/core'
 import { makeStyles } from "@material-ui/core/styles"
 import Main from '../images/Main.svg'
-
+import Box from "@material-ui/core/Box"
 
 const useStyles = makeStyles((theme) => {
-  console.log(theme)
   return ({
   main: {
     width: '100%',
+    paddingBottom: '50px',
     display: 'flex',
     backgroundColor: theme.palette.background.paper,
   },
   text: {
-    width: '40%',
     padding: '100px',
     paddingLeft: '10%',
     color: theme.palette.primary.contrastText,
   },
   image: {
-    width: '60%',
     padding: '0px',
-    paddingRight: '10px',
+    paddingRight: '50px',
     color: theme.palette.primary.contrastText
   },
   }
@@ -32,17 +30,17 @@ const MainMenu = () => {
   const classes = useStyles()
   return (
     <div className={classes.main}>
-      <div className={classes.text}>
+      <Box className={classes.text}>
         <Typography variant='h1'><b>Desarrollo de Software a medida</b></Typography>
         <Typography variant='h2'>consectetur adipiscing elit. Nulla egestas augue vitae lacinia pretium</Typography>
         <div>
           <Button color='secondary'>Botón</Button>
           <Button color='secondary'>Botón</Button>
         </div>
-      </div>
-      <div className={classes.image}>
+      </Box>
+      <Box className={classes.image} display={{xs: 'none', lg: 'block'}}>
         <img src={Main} />
-      </div>
+      </Box>
     </div>
   )
 }
