1c5d5f97e5d28d9d24c97e17226c0cc06f1bc6b0
Japsz <benjamin.meneses.14@sansano.usm.cl>
2020-09-29 18:56:02 -0300

    Responsive Navbar
    - Navbar is now responsive

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,41 +1,16 @@
 import React from "react"
-import { makeStyles } from '@material-ui/core/styles';
-import svg from '../images/Logo.svg'
-import {AppBar, Toolbar, Button, IconButton} from "@material-ui/core"
-import MainMenu from './MainMenu'
+import NavBar from "../components/NavBar"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  icon: {
-    height: '64px',
-  },
-  navLink: {
-    margin: '0px 22px',
-  },
-  flex: {
-    flexGrow: 1,
-  },
-}))
-
+const items = [
+  'Servicios',
+  'Clientes',
+  'Metodología',
+  'Contacto',
+]
 const Landing = () => {
-  const classes = useStyles()
   return (
     <div>
-      <AppBar color='primary' position="sticky">
-        <Toolbar >
-          <IconButton color="inherit" aria-label="menu" edge="start" >
-            <img src={svg} alt="Logo" className={classes.icon}/>
-          </IconButton>
-          <span className={classes.flex}></span>
-          <Button color="inherit" className={classes.navLink}>Servicios</Button>
-          <Button color="inherit" className={classes.navLink}>Clientes</Button>
-          <Button color="inherit" className={classes.navLink}>Metodología</Button>
-          <Button color="inherit" className={classes.navLink}>Contacto</Button>
-        </Toolbar>
-      </AppBar>
-      <MainMenu/>
+      <NavBar items={items}/>
     </div>
   )
 }
