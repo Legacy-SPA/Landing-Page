fbeded2fb55359a86563d3c17393c6d23378674d
Japsz <benjamin.meneses.14@sansano.usm.cl>
2020-10-20 18:46:49 -0300

    Renamed App root page to index

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,11 +11,11 @@ exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
-  if (page.path.match(/^\/App/)) {
+  if (page.path.match(/^\/Index/)) {
     page.matchPath = "/*"
     // Update the page.
     createPage(page)
   } else {
-    
+
   }
 }
