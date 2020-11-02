01622b2f2d65e681a0bd64aec3e4701d98cdc85e
Japsz <benjamin.meneses.14@sansano.usm.cl>
2020-11-02 17:45:27 -0300

    Improvements
    - No more Newsletter
    - Fixed fontWeight to 400
    - Centered ContactForm

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Grid } from "@material-ui/core"
-import News from "./News"
 import ContactForm from "./ContactForm"
+import Box from "@material-ui/core/Box"
 
 const Contact = (props) => (
   <Grid container xs={12} {...props}>
-    <News />
-    <ContactForm />
+    <Box flexGrow={1} display="flex" justifyContent="center">
+      <ContactForm />
+    </Box>
   </Grid>
 )
 
