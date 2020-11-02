1f924d6a195eb1afc16c5768398fddef0f2cae6b
Japsz <benjamin.meneses.14@sansano.usm.cl>
2020-11-02 18:04:18 -0300

    Services Padding

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
 import { Typography, Grid } from "@material-ui/core"
 import cms from "../images/cms.svg"
 import layers from "../images/layers.svg"
@@ -7,6 +7,7 @@ import project from "../images/project.svg"
 import cloud from "../images/clients/Cloud.svg"
 import proyecto from "../images/clients/Proyecto.svg"
 import appDevelopment from "../images/appDevelopment.svg"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -30,7 +31,8 @@ const useStyles = makeStyles(theme => ({
   },
   serviceDesc: {
     color: theme.palette.primary.main,
-    marginTop: 15
+    marginTop: 15,
+    paddingRight: 10,
   },
   contentServices: {
     padding: "0 10% 50px 10%",
@@ -54,7 +56,10 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const Services = (props) => {
+  const theme = useTheme();
   const classes = useStyles()
+  const isSM = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMD = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <Grid container {...props}>
       <Grid item xs={12} className={classes.title}>
@@ -65,8 +70,8 @@ const Services = (props) => {
           Brindamos servicios informáticos integrales, para suministrar la próxima generación de app en la web, dispositivos móviles y de escritorio:{" "}
         </Typography>
       </Grid>
-      <Grid container item xs={12} className={classes.contentServices}>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+      <Grid container xs={12} className={classes.contentServices} spacing={isSM ? 0 : isMD ? 3 : 5}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img src={project} alt={"Proyecto"} />
           <Typography className={classes.serviceDesc} variant="h2">
             <b>Auditoria e Intervencion de Software</b>
@@ -75,7 +80,7 @@ const Services = (props) => {
             Llevamos tu idea de negocio, o tu negocio al siguiente nivel, para ello te prestamos la asesoría en base a consultorías con un equipo de profesionales como mas de 20 años de experiencia.
           </Typography>
         </Grid>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img alt={"Software"} src={cms} />
           <Typography className={classes.serviceDesc} variant="h2">
             <b>Desarrollo de Software a medida</b>
@@ -84,7 +89,7 @@ const Services = (props) => {
           Bien sea que quieras modificar tu sistema o quieras hacer uno, te llevamos de la mano, aportando valor a tu negocio y desarrollando el software que se adapte a tus necesidades. Sin perder calidad ni rendimiento.
           </Typography>
         </Grid>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img alt={"Apps"} src={appDevelopment} />
           <Typography className={classes.serviceDesc} variant="h2">
             <b>Desarrollo de aplicaciones móviles</b>
@@ -93,7 +98,7 @@ const Services = (props) => {
           Contamos con el equipo ideal para conceptualizar tu idea o transformar tu negocio en una app móvil que permitan a tus usuario interactuar contigo desde el mundo de los smart phones. Te brindamos soluciones escalables y transformables en el tiempo.
           </Typography>
         </Grid>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img alt={"Integración"} src={layers} />
           <Typography className={classes.serviceDesc} variant="h2">
             <b>Servicio de Equipo de Desarrollo Dedicado.</b>
@@ -102,7 +107,7 @@ const Services = (props) => {
             ¿Quiere desarrollar un nuevo software o mejorar el que tienes?. Ya no tienes porque preocuparte por tener un área TI, Nosotros te asignamos una célula ágil de desarrollo y te ayudamos a que alcances el siguiente nivel desde lo tecnologico.
           </Typography>
         </Grid>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img src={proyecto} alt={"Software"}  style={{width: 52, height: 49}} />
           <Typography className={classes.serviceDesc} variant="h2">
             <b>Automatizacion robotica de procesos (RPA)</b>
@@ -111,7 +116,7 @@ const Services = (props) => {
             Con los robots tecnológicos (RPA), te potenciamos como empresa al poder automatizar muchísimos procesos, simulando un comportamiento humano.
           </Typography>
         </Grid>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img src={cloud} alt={"Apps"} style={{width: 52, height: 49}}/>
           <Typography className={classes.serviceDesc} variant="h2">
             <b>DevOps</b>
@@ -120,7 +125,7 @@ const Services = (props) => {
           Quieres migrar la arquitectura de tus aplicaciones a una arquitectura robusta, escalable y mas económica, tenemos la solución para ti, con nuestra experiencia tendrás en tus aplicaciones el mejor rendimiento al menor costo.
           </Typography>
         </Grid>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img src={cloud} alt={"Apps"} style={{width: 52, height: 49}}/>
           <Typography className={classes.serviceDesc} variant="h2">
             <b>Blockchain</b>
@@ -129,7 +134,7 @@ const Services = (props) => {
           Conectamos tu empresa con el mundo Block Chain, automatizamos tus procesos de compra/venta de criptomonedas, para que ellos trabajen por ti.
           </Typography>
         </Grid>
-        <Grid xs={12} sm={6} lg={4} className={classes.itemService}>
+        <Grid item xs={12} sm={6} lg={4} className={classes.itemService}>
           <img src={cloud} alt={"Apps"} style={{width: 52, height: 49}}/>
           <Typography className={classes.serviceDesc} variant="h2">
             <b>Inteligencia Artificial</b>
